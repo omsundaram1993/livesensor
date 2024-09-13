3da807c6b1c8f43f341be68a3f08314c164e95ea
Om Sundaram <omsundaram1993@gmail.com>
2024-09-13 08:47:19 +0530

    first commit

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@ from setuptools import find_packages , setup
 #from typing import List
 
 
+def get_requirements() ->list[str]:
+    requirements_list = list[str] = []
+    return requirements_list
+
+
 setup(
     name='sensor',
     version="0.0.1",
     author="Om",
     author_email='omsundaram1993@gmail.com',
     packages=find_packages(),
-    install_requires=["pymongo"]
+    install_requires=get_requirements()
 
 )
